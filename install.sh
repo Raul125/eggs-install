5dd4026e351d51fbc7e9ec9739dcc96b12a9aee7
Raúl R. <raul125esp@gmail.com>
2025-05-21 10:13:52 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "###############################################################"
+echo "#                       Neon Community                        #"
+echo "###############################################################"
+
+# Install dependencies
+apt-get update && apt-get install -y unzip libicu-dev lib32gcc-s1 && apt-get clean && rm -rf /var/lib/apt/lists/*
+
+# Remove old binaries and any potential leftovers
+rm -rf /mnt/server/.bin
+mkdir -p /mnt/server/.bin/SteamCMD
+cd /mnt/server/.bin/SteamCMD
+
+# Download SteamCMD
+curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxf -
+
+# Download SCP:Secret Laboratory Dedicated Server (with reduced output)
+./steamcmd.sh +force_install_dir /mnt/server/.bin/SCPSLDS +login anonymous +app_update 996560 -beta "$BETA_NAME" $( [ "$BETA_PASSWORD" != "none" ] && echo "-betapassword $BETA_PASSWORD" ) validate +quit
+
+# Remove installation files and temporary files that might have been created
+rm -rf /mnt/server/.bin/SteamCMD
+rm -rf /tmp/dumps
+rm -f /root/Steam*
+
+echo "###############################################################"
+echo "#                   Installation completed!                   #"
+echo "###############################################################"
